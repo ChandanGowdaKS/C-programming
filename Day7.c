fbe8284c348c7d84dc941d308b66eaa2b6428488
Chandan Gowda K S <chanduchess122@gmail.com>
2024-10-03 21:18:52 +0530

    Add files via upload

---
+++ Day7.c
@@ -0,0 +1,47 @@
+// #include<stdio.h>
+// #include <math.h>
+// #include <stdlib.h>
+
+// void main(){
+//     char ch;
+//     char s[100];
+//     char sen[100];
+    
+//     printf("enter the first input \n");
+//     scanf("%c",&ch);
+//     printf("enter the second input \n");
+//     scanf("%s",&s);
+//     printf("enter the Third input \n ");
+//     scanf("%s",&sen);
+    
+//     printf("%c\n%s\n%s",ch,s,sen);
+    
+    
+// }
+
+// #include<stdio.h>
+// void sum();         // Function Delcaration
+
+// void sum(){             // Function Definition(definning)
+//     int a = 10, b = 20, add;
+
+//     printf("%d",a+b);
+// }
+
+// void main(){                // Function Calling
+//     sum();
+// }
+
+// #include<stdio.h>
+// int add();
+
+// int add(){
+//     int a = 10, b = 10;
+//     printf("%d",a+b);
+//     return 0;
+// }
+
+// int main(){
+//     add();
+//     return 0;
+// }
