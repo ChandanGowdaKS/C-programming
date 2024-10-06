6ffa504b7bd67fbf5c8271cc24466ac05c2facb5
Chandan Gowda K S <chanduchess122@gmail.com>
2024-10-06 18:55:56 +0530

    Add files via upload

---
+++ Cprog.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+	int a[5],i;
+	
+	for (i=0;i<5;i++){
+	printf("Enter The %dst Number: \n",i);
+	scanf("%d",&a[i]);
+	}
+	for(i=0;i<5;i++){
+	printf("The Number is %d\n",a[i]);
+	
+	}
+	}
+	
